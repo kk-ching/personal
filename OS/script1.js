0c14df9bfd5c03bfdf200d20c7a7bd5647590b2e
kkching0311 <smallk67red@gmail.com>
2025-02-16 02:45:28 +0800

    the browser page

---
+++ OS/script1.js
@@ -0,0 +1,42 @@
+const OUTPUT = document.getElementById("OS");
+let output_str = "";
+let os, browser;
+function keyword(ketword) {
+    let useragent_str = window.navigator.userAgent;
+    let bool = useragent_str.includes(ketword);
+    return bool
+}
+
+
+function check() {
+
+
+    if (keyword("Chrome")) {
+        browser = "chrome";
+    } else if (keyword("Firefox")){
+        browser = "firefox";
+    }else if (keyword("Safari")){
+        browser = "Safari";
+    }else if (keyword("Edge")) {
+        browser = "Edge";
+    }else if (keyword("Opera")){
+        browser = "opera";
+    }
+
+
+    if(keyword("Windows")){
+        os = "Windows";
+    }else if (keyword("Linux")){
+        if(keyword("Android")){
+            os = "Android";
+        } else {
+            os = "Linux";
+        }
+    }else if(keyword("Macintosh")) {
+        os = "Mac OS X"
+    }else if(keyword("iPhone") || keyword("iPad")) {
+        os = "IOS";
+    }
+
+    OUTPUT.innerHTML = "Your using " + browser + " on " + os;
+}
